5c6c6b7a8d186214242ff2e330fd95abdb15d656
Malik Glossop <malik.glossop@gmail.com>
2018-08-04 14:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(array,kitten) {
-  array.push(kitten)
-  return array
+  kittens.push(kitten)
+  return kittens
 }
